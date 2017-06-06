026414f4b2fe99d5fcbbbab8a190dc96f563a7f7
Mark Brouch <mbrouch@walmartlabs.com>
2017-06-05 22:28:23 -0500

    🐛 Fix resolve bug

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const resolve = require('resolve');
 module.exports = function(options) {
   return {
     resolve(filename, source, pugOptions) {
-      return resolve.sync(filename, Object.assign({
+      const file = path.parse(filename);
+      const resolvePath = file.dir ? `${file.dir}/${file.name}` : file.name;
+
+      return resolve.sync(resolvePath, Object.assign({
         basedir: path.dirname(source),
         extensions: ['.pug', '.jade'],
         packageFilter: pkg =>
