4beba2ef7968991681458f65696f198da71f5f1a
Mark Brouch <mbrouch@walmartlabs.com>
2017-06-06 22:46:08 -0500

    💄 Format with Prettier

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const path = require('path');
-const resolve = require('resolve');
+const path = require("path");
+const resolve = require("resolve");
 
 module.exports = function(options) {
   return {
@@ -7,25 +7,31 @@ module.exports = function(options) {
       const file = path.parse(filename);
 
       // Strip out extension that is automatically added by Pug
-      file.ext = '';
+      file.ext = "";
       file.base = file.name;
 
       let resolvePath;
-      if (!file.dir && file.dir[0] !== path.sep && file.dir[0] !== '.') {
+      if (!file.dir && file.dir[0] !== path.sep && file.dir[0] !== ".") {
         resolvePath = file.name;
       } else {
         resolvePath = path.format(file);
       }
 
-      return resolve.sync(resolvePath, Object.assign({
-        basedir: path.dirname(source),
-        extensions: ['.pug', '.jade'],
-        packageFilter(pkg) {
-          return Object.assign({}, pkg, {
-            main: pkg.pug || pkg.main
-          });
-        }
-      }, options));
+      return resolve.sync(
+        resolvePath,
+        Object.assign(
+          {
+            basedir: path.dirname(source),
+            extensions: [".pug", ".jade"],
+            packageFilter(pkg) {
+              return Object.assign({}, pkg, {
+                main: pkg.pug || pkg.main
+              });
+            }
+          },
+          options
+        )
+      );
     }
   };
 };
